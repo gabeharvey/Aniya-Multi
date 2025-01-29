e005ed1be0b6c208718a36691aaade4e636c3095
Gabe Harvey <harvey7@gmail.com>
2025-01-29 09:34:17 -0600

    Initial Commit

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,45 @@
+import PlayerBio from './PlayerBio';
+import PlayerCards from './PlayerCards';
+import Contact from './Contact';
+import StatTracker from './StatTracker';
+import PlayerFilm from './PlayerFilm';
+import Offers from './Offers';
+import Media from './Media';
+import Academics from './Academics';
+import AthleteProgression from './AthleteProgression';
+
+const MainLayout = () => {
+  return (
+    <>
+      <div id="player-bio">
+        <PlayerBio />
+      </div>
+      <div id="player-film">
+        <PlayerFilm />
+      </div>
+      <div id="player-cards">
+        <PlayerCards />
+      </div>
+      <div id="athlete-progression">
+        <AthleteProgression />
+      </div>
+      <div id="stat-tracker">
+        <StatTracker />
+      </div>
+      <div id="media">
+        <Media />
+      </div>
+      <div id="academics">
+        <Academics />
+      </div>
+      <div id="offers">
+        <Offers />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
